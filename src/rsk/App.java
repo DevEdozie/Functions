43a366e9acadbf60866cdd85dffcd7a21a67c475
nicholaus michael <nicholausmichael51@gmail.com>
2022-06-24 21:33:50 +0100

    gdsc scholarship day 2

---
+++ src/rsk/App.java
@@ -0,0 +1,8 @@
+package rsk;
+
+public class App {
+    public static void main(String[] args) {
+        DisplayFunctions.log("Hello World from Java",3);
+        DisplayFunctions.log("Hello World from Java");
+    }
+}
